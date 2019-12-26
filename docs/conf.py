61a0ea1f02a942bf30ba8b989522136ddd48de49
Snownee <1850986885@qq.com>
2019-12-26 11:50:05 +0800

    .

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ author = 'Snownee'
 
 # -- General configuration ---------------------------------------------------
 
+source_suffix = ['.rst', '.md']
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
